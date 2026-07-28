937ddf75922903772aa418ebb527d399c1af79e9
Nithin Bharadwaj <tsrnb@air.local>
2026-07-28 13:50:59 +0530

    Deploy Flutter Web build/web from master

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4146760814"
+    serviceWorkerVersion: "1854347290"
   }
 });
