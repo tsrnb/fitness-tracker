1c59c02744758ef1e611232969e4eabfba69f401
Nithin Bharadwaj <tsrnb@air.local>
2026-07-30 02:32:01 +0530

    Deploy Flutter Web build/web from master

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "da9d710cd0fbc790d186bf48a0b45df0",
+const RESOURCES = {"flutter_bootstrap.js": "6cbb3f6698918868ddb0c208e9171421",
 "version.json": "4aca11a9b2ba9208d10a2ff2b5e820b3",
 "index.html": "b7b932465923ef67405ae0cb74c1dcf4",
 "/": "b7b932465923ef67405ae0cb74c1dcf4",
-"main.dart.js": "94a3fe45eff3ccbf372352b6e0b917fb",
+"main.dart.js": "c614d6a3adfdcb25ae5199d61abdc82d",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
