bc42bcc681ce8ba42186809e84a2376f10464821
Nithin Bharadwaj <tsrnb@air.local>
2026-08-17 12:54:54 +0530

    Deploy Flutter Web build/web from master

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2448612580"
+    serviceWorkerVersion: "3432183855"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "af4511b4c8322d1ac793d653e13427a3",
+const RESOURCES = {"flutter_bootstrap.js": "baede1b76e737d8efb10a7bf30b68a73",
 "version.json": "4aca11a9b2ba9208d10a2ff2b5e820b3",
 "index.html": "b7b932465923ef67405ae0cb74c1dcf4",
 "/": "b7b932465923ef67405ae0cb74c1dcf4",
-"main.dart.js": "58c2daf7cd58c72cfc4535be6a70899d",
+"main.dart.js": "0e0834221d8146dd6c194d08825b5f61",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
